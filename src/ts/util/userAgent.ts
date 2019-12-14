1851894da6f680c51a6ac9101e73f9e436ff0a1a
sunnymerryAce <y.hirano.0104@gmail.com>
2019-12-14 11:19:20 +0900

    [fix] useAgent

--- src/ts/util/userAgent.ts
+++ src/ts/util/userAgent.ts
@@ -1,11 +1,9 @@
 const ua: string = navigator.userAgent.toLowerCase();
 
-export const isSp: boolean = (() => /iphone|ipod|android/.test(ua))();
+export const isSp: boolean = /iphone|ipod|android/.test(ua);
 
-export const isIE: boolean = (() => /msie|trident/.test(ua))();
+export const isIE: boolean = /msie|trident/.test(ua);
 
-export const isIOS: boolean = (() => /iphone|ipad|ipod/.test(ua))();
+export const isIOS: boolean = /iphone|ipad|ipod/.test(ua);
 
-export const isSafari: boolean = (() => {
-  return /safari/.test(ua) && !/chrome/.test(ua);
-})();
+export const isSafari: boolean = /safari/.test(ua) && !/chrome/.test(ua);
